d4b64c1b635ec3775ee5964343e59b07786058c9
Joy Leelawat <joyride80@users.noreply.github.com>
2026-04-09 13:52:20 +0200

    Fix: add trailing slash to base path for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import mdx from '@astrojs/mdx';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://joyride80.github.io',
-  base: '/joyObjectSite',
+  base: '/joyObjectSite/',
   devToolbar: {
     enabled: false,
   },
